dcfe6fcc53c622b5f6710fe61f500ee18055acb1
SoftPortable 76 <83284943+hab76DZ@users.noreply.github.com>
2023-04-13 16:48:33 +0200

    Update bbump.js

--- commands/utils/bbump.js
+++ commands/utils/bbump.js
@@ -4,7 +4,7 @@ module.exports = [{
     type: "interaction", 
     code: `
   $interactionReply[;{newEmbed:{title:Boost Bump avec succès !}{description:J'ai boost bumper ton serveur avec succès sur mon serveur support avec mention ! Attend **24H** pour recommencer le bump !}{footer: RenFox 76 }{image:https://cdn.discordapp.com/attachments/1066723253562900521/1067028962053718036/Picsart_23-01-23_11-31-24-186.png}};;;everyone;false]
-  $channelSendMessage[1079684071359258634;Mention = <@&1072984973243584522> {newEmbed: {title:**Serveur Boost Bump**}{description: 
+  $channelSendMessage[ID DU SALON;Mention = <@&1072984973243584522> {newEmbed: {title:**Serveur Boost Bump**}{description: 
   > Nom du serveur: ** [$guildName]($getGuildInvite[$guildID]) **
   > Nombres de membres : ** $membersCount **
   > Boost Bump effectué par ** $userTag **
